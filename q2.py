a5b392166085ea0ff4f5bd18f32a9b959697db55
vigna-s <vigna.surapaneni@gmail.com>
2022-03-17 14:48:38 +0530

    B of Part-B works, has an issue where response length is -(and not a number)

--- q2.py
+++ q2.py
@@ -3,16 +3,14 @@ import pyspark,re
 
 #first part, loading into RDD
 spark = pyspark.sql.SparkSession.builder.appName("Part B").getOrCreate()
-rdd = spark.sparkContext.textFile("../access.log")
+rdd = spark.sparkContext.textFile("../access.log") #need to change this line later
 
 #second part, parsing with Regex and Spark
 tmp1 = rdd.flatMap(lambda x: x.split("-"))
 tmp2 = tmp1.flatMap(lambda x: x.split("\""))
 #tmp3 = tmp2.flatMap(lambda x:x.split(" "))
 tmp3 = tmp2.filter(lambda x: x != ' ' and x != '')
-print(tmp3.take(20))
 tmp4 = tmp3.flatMap(lambda x: re.split(r"\s+",x,maxsplit=1))
-print(tmp4.take(20))
 
 #timestamps
 print("printing timestamps")
@@ -25,21 +23,28 @@ print("printing host addresses")
 ha = tmp4.filter(lambda x: re.match(r'.+\..+\..+\..+',x) and re.match(r'^\d',x))
 print(ha.take(10))
 
-tmp5= tmp4.filter(lambda x: x != ' ' and x != '')
-#print(tmp5.take(20))
-tmp6 = tmp5.filter(lambda x: not re.match(r'.+\..+\..+\..+',x) and re.match(r'^\d',x))
-print(tmp6.take(20))
+#request type
+print("printing requests")
+req = tmp4.filter(lambda x: re.match(r'[A-Z]+$',x))
+print(req.take(10))
 
-sp1 = tmp6.flatMap(lambda x: x.split(" "))
-print(sp1.take(20))
-sp2 = sp1.filter(lambda x: x!='')
 
-# tmp6 = tmp5.filter(lambda x: not re.match(r'^\/',x))
-# print(tmp6.take(20))
-# tmp7 = tmp6.filter(lambda x: not re.match(r'^\(',x))
-# print(tmp7.take(20))
-# tmp8 = tmp7.filter(lambda x: not re.match(r'^http',x))
-# print(tmp8.take(20))
 
+tmp5 = tmp4.filter(lambda x: re.match(r'^([\s\d]+)$',x))
+tmp6 = tmp5.filter(lambda x: not re.match(r'^([\d]+)$',x))
+sp1 = tmp6.flatMap(lambda x: re.split(r"\s+",x,maxsplit=1))
+
+print("printing response codes")
+codes = sp1.filter(lambda x: re.match(r'^([\d]+)$',x))
+print(codes.take(10))
+
+sp2 = sp1.filter(lambda x: not re.match(r'^([\d]+)$',x))
+sp3 = sp2.flatMap(lambda x: x.split(" "))
+
+print("printing response lengths")
+reslen = sp3.filter(lambda x: x!= '')
+print(reslen.take(10))
+
+print(reslen.count())
 
 
